0e8e5a2ddc52415c6c1dae4c9c4cc4b2e9768d09
VitamiN <scoundrel98@gmail.com>
2021-03-04 22:30:09 +0300

    create database on app init

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "sinatra/reloader"
+gem "sqlite3"
+
--- app.rb
+++ app.rb
@@ -3,6 +3,20 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/contrib'
 require 'pony'
+require 'sqlite3'
+
+configure do
+	@db = SQLite3::Database.new 'barber.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS "Users"
+    (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "username" TEXT,
+      "phone" TEXT,
+      "datestamp" TEXT,
+      "barber" TEXT,
+      "color" TEXT
+    )'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"			
